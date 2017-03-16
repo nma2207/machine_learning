97b844551d16141c66fbfff4b6bb2ab44097179c
Marat Nabiev <nma2207@gmail.com>
2017-03-16 07:34:24 +0300

    Aaa

--- regression.py
+++ regression.py
@@ -30,30 +30,39 @@ def calculate_y(x):
 
 def calculate_t(x):
     y=calculate_y(x)
-    e = 10 * np.random.randn(1000)
+    e = 10 * np.random.randn(x.size)
     t = y + e
     return t
 
-def find_min_error(training_x, training_set, valid_x, valid_set, test_x, test_set):
+def find_min_error(training_x, training_set, valid_x, valid_set, exp_count):
     #все это надо переделать, но мне пока лень
-    errors=np.zeros((n,lams))
-    for i in range(n):
-        for lam in range(lams):
-            F=computing_F(x, i)
-            w=computing_w(F,t, lam)
-            #new_y=F.dot(w)
-            print w.shape
-            #типа штуки с кросс-валидацией
-            x1 = np.linspace(0, .1, 200)
-            y = 20 * np.sin(2 * np.pi * 3 * x1) + 100 * np.exp(x1)
-            e = 10 * np.random.randn(200)
-            t1 = y + e
-            #считаем по заданному
-            new_y=computing_y(w, x1)
-            errors[i,lam]=np.sum((new_y - t1) ** 2)
+    train_errors=np.zeros((exp_count))
+    errors=np.zeros((exp_count))
+    data=np.zeros((exp_count, 2))
+    for i in range(exp_count):
+        n=int(np.random.uniform(1,100,1))
+        lam=np.random.uniform(-50,50,1)
+        F = computing_F(training_x, n)
+        w = computing_w(F, training_x, lam)
+        train_y=computing_y(w,training_x)
+        valid_y=computing_y(w, valid_x)
+        train_errors[i]=np.sum((train_y-training_set)**2)
+        errors[i]=np.sum((valid_y-valid_set)**2)
+        data[i][0]=n
+        data[i][1]=lam
+    plt.figure()
+    print data[:,0]
+    plt.plot(data[:,0], train_errors,'g')
+    plt.plot(data[:, 0], errors, 'b')
+    plt.show()
     find=np.where(errors == np.min(errors))
-    print np.min(errors), np.where(errors==np.min(errors))
-    return find[0], find[1]
+    index=find[0][0]
+    n=data[index][0]
+    lam=data[index][1]
+    print data[index]
+    F = computing_F(training_x, n)
+    w = computing_w(F, training_x, lam)
+    return w
 
 
 
@@ -73,25 +82,29 @@ def main():
     test_x=np.random.uniform(0,1,200)
     test__set=calculate_t(test_x)
 
+    w = find_min_error(training_x, training_set, valid_x, valid_set, 1000)
 
-
+    new_x=np.linspace(0,1,500)
+    new_y=computing_y(w,new_x)
 
     plt.figure()
     plt.plot(x, y, 'r')
     plt.plot(training_x,training_set, '.g')
+    plt.plot(new_x, new_y, 'g')
+    plt.show()
 
 
-    n,lams=find_min_error(x,t,30, 10)
+
     #на случай, если таких будет несколько
-    for i in range(n.size):
-        F = computing_F(x, n[i])
-        w = computing_w(F, t, lams[i])
-        new_y = F.dot(w)
+    # for i in range(n.size):
+    #     F = computing_F(x, n[i])
+    #     w = computing_w(F, t, lams[i])
+    #     new_y = F.dot(w)
 
     #plt.plot(np.arange(k), errors, 'g')
 
-    plt.plot(x,new_y,color=(float(i)/n.size,0.,1. ))
-    plt.show()
+    # plt.plot(x,new_y,color=(float(i)/n.size,0.,1. ))
+    # plt.show()
 
 if __name__=="__main__":
     main()
